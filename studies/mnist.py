29de08bd0becad5308b186c537b8831d832f6a2c
Arthur Menezes <arthuramsouza@gmail.com>
2018-10-13 16:34:44 -0300

    MNIST study case: model implemented.

--- studies/mnist.py
+++ studies/mnist.py
@@ -14,7 +14,6 @@ plt.imshow(data.train.images[1].reshape(784, 1), cmap='gist_gray', aspect=0.02)
 plt.show()
 
 # model
-
 x = tf.placeholder(tf.float32, shape=[None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
